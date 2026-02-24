8b6f70e28d18334890af4f6b61d45831cdf32b00
liegeasmathieu-lgtm <liegeas-mathieu@orange.fr>
2026-02-24 20:21:35 +0100

    debug2

--- src/components/supabaseclient.js
+++ src/components/supabaseclient.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Ces variables doivent être configurées dans ton interface Vercel
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// TEST TEMPORAIRE (Ne pas laisser en production)
+const supabaseUrl = "https://cvxqsvjfvvozdezjrbmq.supabase.co" 
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2eHFzdmpmdnZvemRlempyYm1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3ODY1NzEsImV4cCI6MjA4NzM2MjU3MX0.kOJ2KjwSgm0SA1xaugI_62wGFot6HFxR7HS2rC1lTbs"
+
 console.log("Vite est en train de charger l'URL :", import.meta.env.VITE_SUPABASE_URL ? "DÉFINIE" : "VIDE");
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
