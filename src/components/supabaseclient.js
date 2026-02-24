785bfbe3025255c5b63489faf45e017de40a3dc6
liegeasmathieu-lgtm <liegeas-mathieu@orange.fr>
2026-02-24 20:17:50 +0100

    Debug env vars

--- src/components/supabaseclient.js
+++ src/components/supabaseclient.js
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 // Ces variables doivent être configurées dans ton interface Vercel
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
+console.log("Vite est en train de charger l'URL :", import.meta.env.VITE_SUPABASE_URL ? "DÉFINIE" : "VIDE");
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
